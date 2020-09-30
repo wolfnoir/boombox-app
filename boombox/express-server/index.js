7f7ba872622324b7e5167099bca279164825bbec
Billy Wong <billy.wong@stonybrook.edu>
2020-09-30 17:34:22 -0400

    added express app

---
+++ boombox/express-server/index.js
@@ -0,0 +1,26 @@
+const path = require("path");
+const express = require("express");
+const app = express(); // create express app
+
+// add middlewares
+app.use(express.static(path.join(__dirname, "..", "build")));
+app.use(express.static("public"));
+
+/*
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+});
+*/
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+});
+
+app.get("/helloworld", (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "build", "helloworld.html"));
+});
+
+// start express server on port 5000
+app.listen(5000, () => {
+  console.log("server started on port 5000");
+});
