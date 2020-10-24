218ade447e4beaf9a38bbacb1ea8c749b79768c6
Everett Yang <amber.yang@stonybrook.edu>
2020-10-24 13:35:28 -0400

    Fixed fullscreen background

--- boombox/src/Login.js
+++ boombox/src/Login.js
@@ -4,7 +4,22 @@ import './Login.css'
 function Login() {
     return (
         <div className = "loginScreen">
-              
+              <center>
+
+                <div className = "boombox-header">
+                    Boombox
+                </div>
+
+                <div className = "boombox-subtitle">
+                    putting personality back into playlists
+                </div>
+
+                <div className = "login-container">
+
+                </div>
+
+
+              </center>
               <a className = "freepix-attribution" href='https://www.freepik.com/vectors/background'>Background vector created by pikisuperstar - www.freepik.com</a>
         </div>
     );
