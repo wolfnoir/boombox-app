c9035e55b2388d80762a3f0c13bcaeed31f14ef0
Billy Wong <billy.wong@stonybrook.edu>
2020-10-24 18:51:34 -0400

    setup placeholder routes for the express server

--- boombox/express-server/index.js
+++ boombox/express-server/index.js
@@ -1,17 +1,114 @@
 const path = require("path");
 const express = require("express");
+const session = require("express-session");
+const bodyParser = require("body-parser");
 const app = express(); // create express app
 
 // add middlewares
 app.use(express.static(path.join(__dirname, "..", "build")));
 app.use(express.static("public"));
 
-// Non-React routes (take priority over React routes)
+app.use(session({
+	secret: 'sdh78g873huriniw',
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+
+/*-----------------------------------------------------
+Non-React routes (take priority over React routes)
+-------------------------------------------------------*/
+
 app.get("/helloworld", (req, res) => {
     res.sendFile(path.join(__dirname, "..", "build", "helloworld.html"));
 });
 
-// React routes
+app.post("/loginUser", (req, res) => {
+    //request.session.loggedin = true;
+    //request.session.username = "";
+});
+
+app.post("/logoutUser", (req, res) => {
+    //request.session.destroy();
+});
+
+app.post("/editUsername", (req, res) => {
+	
+});
+
+app.post("/editPassword", (req, res) => {
+	
+});
+
+app.post("/editProfileImage", (req, res) => {
+	
+});
+
+app.post("/editEmail", (req, res) => {
+	
+});
+
+app.post("/editBio", (req, res) => {
+	
+});
+
+//is a placeholder, can expand on it later
+app.post("/editPlaylist", (req, res) => {
+	
+});
+
+
+
+
+//should these be get, post, or both?
+
+app.get("/getBookmarks", (req, res) => {
+	return {'bookmarks': [{}, {}, {}]};
+});
+
+app.get("/getFollowingNewPlaylists", (req, res) => {
+	return {}
+});
+
+app.get("/getRecommendedPlaylists", (req, res) => {
+	return {}
+});
+
+app.get("/getPlaylist/:playlistId", (req, res) => {
+	//req.params.playlistId
+	return {}
+});
+
+app.get("/getSearchResults", (req, res) => {
+	return {}
+});
+
+app.get("/getProfilePageData", (req, res) => {
+	return {}
+});
+
+app.get("/getFollowers", (req, res) => {
+	return {}
+});
+
+app.get("/getFollowing", (req, res) => {
+	return {}
+});
+
+app.get("/getUserPlaylists", (req, res) => {
+	return {}
+});
+
+
+
+
+
+
+
+/*----------------------------- ------------------------
+React routes
+-------------------------------------------------------*/
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "..", "build", "index.html"));
 });
@@ -20,5 +117,5 @@ app.get("*", (req, res) => {
 
 // start express server on port 5000
 app.listen(5000, () => {
-  console.log("server started on port 5000");
+    console.log("server started on port 5000");
 });
