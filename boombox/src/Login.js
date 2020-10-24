bb0df564ddc6a229f032c7477afc6f4005e40720
Everett Yang <amber.yang@stonybrook.edu>
2020-10-24 13:49:04 -0400

    Aligned content to the center of the page.

--- boombox/src/Login.js
+++ boombox/src/Login.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import './Login.css'
+import './Login.css';
+
 
 function Login() {
     return (
         <div className = "loginScreen">
+            <div className = "login-container">
               <center>
 
                 <div className = "boombox-header">
@@ -14,12 +16,14 @@ function Login() {
                     putting personality back into playlists
                 </div>
 
-                <div className = "login-container">
-
+                <div className = "login-menu">
+                    AAAAAAAAAAAAAAA <br/>
+                    <button>Log In</button>
                 </div>
 
 
               </center>
+            </div>
               <a className = "freepix-attribution" href='https://www.freepik.com/vectors/background'>Background vector created by pikisuperstar - www.freepik.com</a>
         </div>
     );
