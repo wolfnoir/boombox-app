f21b4890528b536990a7380512947ba7fca96fbd
Jim <jimmyxie48@gmail.com>
2020-11-01 20:37:27 -0500

    Created PlaylistHandler for handling MongoDB requests by playlist IDs

---
+++ boombox/express-server/PlaylistHandler.js
@@ -0,0 +1,137 @@
+const path = require("path");
+const express = require("express");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const MongoClient = require('mongodb');
+const crypto = require('crypto');
+
+const mongoUrl = "mongodb+srv://admin:o8chnzxErmyP7sgK@cluster0.avhnr.mongodb.net?retryWrites=true&w=majority";
+const monogDbName = 'boombox';
+const mongoPlaylistCollection = 'playlists';
+
+class PlaylistHandler {
+    static async createPlaylist() {
+        const client = await MongoClient.connect(mongoUrl, {
+            useNewUrlParser: true,  
+            useUnifiedTopology: true
+        }).catch(err => {
+            console.log(err);
+            return -1;
+        });
+
+        if (!client) {
+            console.log("Client is null");
+            return -1;
+        }
+
+        try {
+            var date = new Date();
+            var user_id = req.session.user_id;
+            const collection = client.db(monogDbName).collection(mongoPlaylistCollection);
+
+            await collection.insertOne({
+                com_enabled: false,
+                comments: [],
+                description: '',
+                image_url: '',
+                last_modified: date.getDate(),
+                likes: [],
+                name: 'Untitled',
+                songs: [],
+                tags: [],
+                user_id: user_id,
+            });
+        }
+
+        catch (err) {
+            console.log(err);
+            return -1;
+        }
+
+        finally {
+            client.close();
+        }
+
+        console.log("Success");
+        return 0;
+    }
+
+    static async editPlaylist(id, com_enabled, comments, description, image_url, likes, name, notes, songs, tags) {
+        const client = await MongoClient.connect(mongoUrl, {
+            useNewUrlParser: true,  
+            useUnifiedTopology: true
+        }).catch(err => {
+            console.log(err);
+            return -1;
+        });
+
+        if (!client) {
+            console.log("Client is null");
+            return -1;
+        }
+
+        try {
+            var date = new Date();
+            var user_id = req.session.user_id;
+            var idObject = new MongoClient.ObjectID(id);
+            const collection = client.db(monogDbName).collection(mongoPlaylistCollection);
+
+            const filter = { "_id": idObject };
+
+            const updateDoc = {
+                com_enabled: com_enabled,
+                comments: comments,
+                description: description,
+                image_url: image_url,
+                last_modified: date.getDate(),
+                likes: likes,
+                name: name,
+                songs: songs,
+                tags: tags,
+                user_id: user_id,
+            }
+
+            await collection.updateOne(filter, updateDoc);
+        }
+
+        catch (err) {
+            console.log(err);
+            return -1;
+        }
+
+        finally {
+            client.close();
+        }
+
+        console.log("Success");
+        return 0;
+    }
+
+    static async createPlaylistRoute(req, res) {
+        const success = await UserHandler.createPlaylist();
+
+        res.send({
+            statusCode: success //-1: an error occurred, 0: success, 1: duplicate username or email
+        });
+    }
+
+    static async editPlaylistRoute(req, res) {
+        const id;
+        const com_enabled = false;
+        const comments = [];
+        const description = "I am an edited playlist!";
+        const image_url = "";
+        const likes = [];
+        const name = "Dummy Playlist";
+        const songs = [];
+        const tags = [];
+        const success = await UserHandler.editPlaylist(id, com_enabled, comments, description, image_url, likes, name, notes, songs, tags);
+
+        res.send({
+            statusCode: success //-1: an error occurred, 0: success, 1: duplicate username or email
+        });
+    }
+}
+
+module.exports = PlaylistHandler;
--- boombox/express-server/index.js
+++ boombox/express-server/index.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const MongoClient = require('mongodb');
 const { assert } = require("console");
+const PlaylistHandler = require("./PlaylistHandler");
 const app = express(); // create express app
 
 // add middlewares
@@ -31,12 +32,12 @@ const mongoTagCollection = 'tags';
 Non-React routes (take priority over React routes)
 -------------------------------------------------------*/
 
-app.get("/helloworld", (req, res) => {
+app.all("/helloworld", (req, res) => {
     res.sendFile(path.join(__dirname, "..", "build", "helloworld.html"));
 });
 
 //should be post
-app.get("/registerUser", (req, res) => {
+app.post("/registerUser", (req, res) => {
 	MongoClient.connect(mongoUrl, {useNewUrlParser: true}, (error, client) => {
 		if (error) throw error;
 		const collection = client.db(monogDbName).collection(mongoUserCollection);
@@ -55,7 +56,7 @@ app.get("/registerUser", (req, res) => {
 	res.send("hello");
 });
 
-app.get("/loginUser", (req, res) => {
+app.all("/loginUser", (req, res) => {
     //request.session.loggedin = true;
 	//request.session.username = "";
 	console.log("current user: " + req.cookies.username);
@@ -64,7 +65,7 @@ app.get("/loginUser", (req, res) => {
 	res.send("login test-user");
 });
 
-app.get("/logoutUser", (req, res) => {
+app.all("/logoutUser", (req, res) => {
 	//request.session.destroy();
 	var user = req.cookies.username;
 	console.log('logout: ' + user);
@@ -72,68 +73,26 @@ app.get("/logoutUser", (req, res) => {
 	res.send("logout " + user);
 });
 
-app.post("/editUserSettings", (req, res) => {
+app.post("/editUserSettings", (req, res) => {});
 
-});
+app.all("/getBookmarks", (req, res) => {});
+
+app.all("/getPlaylistData/:playlistId", (req, res) => {});
+
+app.all("/getProfilePageData/:username", (req, res) => {});
+
+app.all("/getFollowers/:username", (req, res) => {});
+
+app.all("/getFollowing/:username", (req, res) => {});
+
+app.all("/getUserPlaylists/:username", (req, res) => {});
+
+app.post("/createPlaylist", PlaylistHandler.createPlaylistRoute);
 
 //is a placeholder, can expand on it later
-app.post("/editPlaylist", (req, res) => {
-	
-});
+app.post("/editPlaylist", PlaylistHandler.editPlaylistRoute);
 
-//should these be get, post, or both?
-
-app.get("/getBookmarks", (req, res) => {
-	var data = {
-		playlists: [
-			{
-				name: "joke playlist",
-				author: "smithman32",
-				num_likes: 32,
-				image_url: "./images/watermelon-wolf.jpg",
-				url: "/playlist/abc"
-			},
-			{
-				name: "o, my love",
-				author: "banishtheknight",
-				num_likes: 23,
-				image_url: "./images/mountain.jpg",
-				url: "/playlist/abc"
-			},
-			{
-				name: "smallest church in the whole mysterious universe",
-				author: "tequilasunset",
-				num_likes: 653,
-				image_url: "./images/disco-church.png",
-				url: "/playlist/abc"
-			},
-			{
-				name: "creeping noir",
-				author: "maltesefalcon",
-				num_likes: 54,
-				image_url: "./images/noir.jpg",
-				url: "/playlist/abc"
-			},
-			{
-				name: "dreams of green",
-				author: "smithman32",
-				num_likes: 765,
-				image_url: "./images/leafy.jpg",
-				url: "/playlist/abc"
-			},
-			{
-				name: "searching for you",
-				author: "sidneyfalco",
-				num_likes: 43,
-				image_url: "./images/noir2.png",
-				url: "/playlist/abc"
-			}
-		]
-	};
-	res.json(data);
-});
-
-app.get("/getNewPlaylists", (req, res) => {
+app.all("/getNewPlaylists", (req, res) => {
 	var data = {
 		playlists: [
 			{
@@ -169,7 +128,7 @@ app.get("/getNewPlaylists", (req, res) => {
 	res.json(data);
 });
 
-app.get("/getRecommendedPlaylists", (req, res) => {
+app.all("/getRecommendedPlaylists", (req, res) => {
 	var data = {
 		playlists: [
 			{
@@ -194,216 +153,13 @@ app.get("/getRecommendedPlaylists", (req, res) => {
 	res.json(data);
 });
 
-app.get("/getPlaylistData/:playlistId", (req, res) => {
-	const playlistId = req.params.playlistId;
-	console.log(playlistId);
-	var data = {
-		image_url: './images/horse.png',
-		name: 'wonderwall 10 hrs',
-		author: 'smithman32',
-		last_modified: 1600850614000,
-		liked_by_user: false,
-		bookmarked_by_user: false,
-		num_likes: 69,
-		tags: ['wonderwall', 'pop', 'why me'],
-		description: 'This playlist is for Deborah, who broke my heart. Please take me back.',
-		songs: [
-			{
-				name: 'Wonderwall',
-				artist: 'Oasis',
-				full_url: 'https://dummy_url.com',
-				notes: '',
-				length: 259 //Note: How do we get this data?
-			},
-			{
-				name: 'Wonderwall',
-				artist: 'Oasis',
-				full_url: 'https://dummy_url.com',
-				notes: 'Why the obsession with Wonderwall? Well, it\'s because it\'s the only song I know how to play.',
-				length: 259 //Note: How do we get this data?
-			},
-			{
-				name: 'Wonderwall',
-				artist: 'Oasis',
-				full_url: 'https://dummy_url.com',
-				notes: '',
-				length: 259 //Note: How do we get this data?
-			},
-			{
-				name: 'Wonderwall',
-				artist: 'Oasis',
-				full_url: 'https://dummy_url.com',
-				notes: '',
-				length: 259 //Note: How do we get this data?
-			}
-		],
-		comments_enabled: true,
-		comments: [
-			{
-				username: 'wonderwall333',
-				content: 'Nice playlist :)',
-				time: 1600854605000
-			},
-			{
-				username: 'wonderwall333',
-				content: 'Nice playlist :)',
-				time: 1600854545000
-			}
-		]
-	}
-	res.json(data);
-});
-
-// for advanced search just pass with get, e.g.  /getSearchResults/wonderwall?author=smithman32
-app.get("/getSearchResults/:query", (req, res) => {
-	var query = req.params.query;
-	var data = {
-		users: [
-			{
-				username: 'wonderwall333',
-				profile_image_url: '/images/luigi time.gif'
-			},
-			{
-				username: 'ilovewonderwall',
-				profile_image_url: './images/kermit.png'
-			},
-			{
-				username: 'wonderwall2',
-				profile_image_url: './images/raccoon.png'
-			}
-		],
-		playlists: [
-			{
-				name: "wonderwall 10 hrs",
-				author: "smithman32",
-				num_likes: 69,
-				image_url: "./images/horse.png",
-				url: "/playlist/abc"
-			}
-		],
-		tags: [
-			'wonderwall', 'oasis', 'alternative', 'rock', 'does anyone actually like wonderwall'
-		]
-	};
-	res.json(data);
-});
-
-app.get("/getProfilePageData/:username", (req, res) => {
-	const username = req.params.username;
-	var data = {
-		username: username,
-		following: false,
-		bio: "Hi, my name is John Smith, and I love making playlists. Deborah, if you're reading this, I'm sorry, and I'm sending you my 10 hour long Wonderwall playlist to make up for it.",
-		playlists: [
-			{
-				name: "joke playlist",
-				author: "smithman32",
-				num_likes: 32,
-				image_url: "./images/watermelon-wolf.jpg",
-				url: "/playlist/abc"
-			},
-			{
-				name: "dreams of green",
-				author: "smithman32",
-				num_likes: 765,
-				image_url: "./images/leafy.jpg",
-				url: "/playlist/abc"
-			},
-			{
-				name: "wonderwall 10 hrs",
-				author: "smithman32",
-				num_likes: 69,
-				image_url: "./images/horse.png",
-				url: "/playlist/abc"
-			}
-		]
-	}
-	res.json(data);
-});
-
-app.get("/getFollowers/:username", (req, res) => {
-	const username = req.params.username;
-	var data = {
-		users: []
-	};
-	for (var i = 0; i < 14; i++) {
-		data.users.push({
-			username: 'wonderwall333',
-			profile_image_url: '/images/luigi time.gif'
-		});
-		data.users.push({
-			username: 'ilovewonderwall',
-			profile_image_url: './images/kermit.png'
-		});
-		data.users.push({
-			username: 'wonderwall2',
-			profile_image_url: './images/raccoon.png'
-		});
-	}
-	res.json(data);
-});
-
-app.get("/getFollowing/:username", (req, res) => {
-	const username = req.params.username;
-	var data = {
-		users: []
-	};
-	for (var i = 0; i < 14; i++) {
-		data.users.push({
-			username: 'wonderwall333',
-			profile_image_url: '/images/luigi time.gif'
-		});
-		data.users.push({
-			username: 'ilovewonderwall',
-			profile_image_url: './images/kermit.png'
-		});
-		data.users.push({
-			username: 'wonderwall2',
-			profile_image_url: './images/raccoon.png'
-		});
-	}
-	res.json(data);
-});
-
-app.get("/getUserPlaylists/:username", (req, res) => {
-	const username = req.params.username;
-	var data = {
-		playlists: [
-			{
-				name: "joke playlist",
-				author: "smithman32",
-				num_likes: 32,
-				image_url: "./images/watermelon-wolf.jpg",
-				url: "/playlist/abc"
-			},
-			{
-				name: "dreams of green",
-				author: "smithman32",
-				num_likes: 765,
-				image_url: "./images/leafy.jpg",
-				url: "/playlist/abc"
-			},
-			{
-				name: "wonderwall 10 hrs",
-				author: "smithman32",
-				num_likes: 69,
-				image_url: "./images/horse.png",
-				url: "/playlist/abc"
-			}
-		]
-	};
-	res.json(data);
-});
-
 
 /*----------------------------- ------------------------
 React routes
 -------------------------------------------------------*/
-app.get("*", (req, res) => {
+app.all("*", (req, res) => {
     res.sendFile(path.join(__dirname, "..", "build", "index.html"));
 });
-
-
 
 // start express server on port 5000
 app.listen(5000, () => {
