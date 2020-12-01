3415ff719a0e1a791af7e9ae5ab5e3db3205528b
Everett Yang <amber.yang@stonybrook.edu>
2020-11-30 20:25:11 -0500

    Added character counter for edit song notes

--- boombox/src/PlaylistEdit.js
+++ boombox/src/PlaylistEdit.js
@@ -388,6 +388,7 @@ class PlaylistEditDisplay extends React.Component {
             data: {},
             song_notes_open: [],
             charCount: 0,
+            noteCharCounts: [],
             imageData: null,
             history: [],
             historyStep: 0,
@@ -417,17 +418,17 @@ class PlaylistEditDisplay extends React.Component {
         })
         .then(res => res.json())
         .then(obj => {
-            if (obj.status == 0) {
+            if (obj.status === 0) {
                 this.setState({data: obj.result});
                 this.setState({currentData: obj.result});
                 for (var i = 0; i < this.state.data.songs.length; i++) {
                     this.state.song_notes_open.push(false);
+                    this.state.noteCharCounts.push(this.state.data.songs[i].note.length);
                 }
                 const songs = this.state.data.songs
                 this.setState ({
                     history: [songs]
                 });
-                console.log(this.state.history);
             }
             else {
                 this.setState({data: null});
@@ -614,6 +615,7 @@ class PlaylistEditDisplay extends React.Component {
         artistField.value = currentSong.artist;
         albumField.value = currentSong.album;
         noteField.value = currentSong.note;
+        this.editSongNote(i);
     }
     
     handleAddTags = (tags) => {
@@ -704,10 +706,17 @@ class PlaylistEditDisplay extends React.Component {
     }
 
     addSongNote() {
-        var songChars = document.getElementById("add-song-char-count");
         var songNote = document.getElementById("add-song-note");
         var length = songNote.value.length;
         this.setState({charCount: length});
+    }
+
+    editSongNote(i) {
+        var songNote = document.getElementById("edit-song-note-" + i);
+        var length = songNote.value.length;
+        var array = this.state.noteCharCounts.slice();
+        array[i] = length;
+        this.setState({noteCharCounts: array});
     }
 
     keyPressed = (e) => {
@@ -935,7 +944,8 @@ class PlaylistEditDisplay extends React.Component {
                                                                     <Col>
                                                                         <Form.Group>
                                                                             <Form.Label>Note</Form.Label>
-                                                                            <Form.Control as="textarea" id = {"edit-song-note-"+i} className = "edit-song-textarea" maxLength = "250" ></Form.Control>
+                                                                            <Form.Control as="textarea" id = {"edit-song-note-"+i} className = "edit-song-textarea" maxLength = "250" onChange = {() => this.editSongNote(i)} ></Form.Control>
+                                                                            <span id = {"edit-song-char-count-" + i}>{this.state.noteCharCounts[i]}</span><span id = "edit-song-max-char">/250</span>
                                                                         </Form.Group>
                                                                     </Col>
                                                                 </Row>
